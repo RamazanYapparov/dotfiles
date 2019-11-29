38a7aba91165534f82c2ba9a164a30d27acb8150
Ramazan Yapparov <ramazan.yapparov@gmail.com>
2019-11-29 22:08:30 +0300

    added touchpad config

--- install.sh
+++ install.sh
@@ -7,5 +7,6 @@ sudo apt install -y vim zsh i3 i3status i3lock && \
 	cd ~/.antigen && \
 	curl -L git.io/antigen > antigen.zsh && \
 	mkdir -p ~/.config/i3 && \
-	cp config ~/.config/i3/
+	cp config ~/.config/i3/ && \
+	find /usr -type d -name 'xorg.conf.d' -exec cp 30-touchpad.conf {} \; -quit
 
