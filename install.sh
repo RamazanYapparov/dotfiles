7957a17bc002fa1a681df750cdb38136da2ce109
Ramazan Yapparov <ramazan.yapparov@gmail.com>
2019-12-09 11:41:02 +0300

    Updated install.sh to use lock.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,9 @@ apt install -y vim zsh i3 i3status i3lock snapd python-pip python3-pip virtualen
 	curl -L git.io/antigen > ~/.antigen/antigen.zsh && \
 	mkdir -p ~/.config/i3 && \
 	cp config ~/.config/i3/ && \
+	mkdir -p ~/bin && \
+	cp lock.sh ~/bin && \
+	chmod +x ~/bin/lock.sh && \
 	mkdir -p /etc/X11/xorg.conf.d/ && \
 	cp 30-touchpad.conf /etc/X11/xorg.conf.d/ && \
 	cp .gitconfig .zshrc .bash_alias ~ && \
