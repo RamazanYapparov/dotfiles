ff544708f0dfae9969217dde916bca608ec37e34
Ramazan Yapparov <ramazan.yapparov@gmail.com>
2019-12-10 10:29:47 +0300

    changed manual copying to rsync; added yota.sh

--- snaps.sh
+++ snaps.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 snap install code --classic && \
-snap install pycharm-professional --classic && \
-snap install intellij-idea-ultimate --classic && \
 snap install insomnia && \
-sudo snap install node --channel=12/stable --classic && \
+snap install intellij-idea-ultimate --classic && \
+snap install node --channel=12/stable --classic && \
+snap install pycharm-professional --classic && \
+snap install skype --classic && \
 snap install sublime-text --classic && \
-snap install telegram-desktop && \
-snap install skype --classic
+snap install telegram-desktop
