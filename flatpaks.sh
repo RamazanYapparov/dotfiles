c901d37debc8bdcb2f1ed0d0a7741fced419e643
Ramazan Yapparov <ramazan.yapparov@gmail.com>
2019-12-11 10:53:30 +0300

    added elementary compatible scripts

---
+++ flatpaks.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+flatpack install -y flathub com.visualstudio.code && \
+flatpack install -y flathub com.jetbrains.IntelliJ-IDEA-Ultimate && \
+flatpack install -y flathub com.jetbrains.PyCharm-Professional && \
+flatpack install -y flathub com.skype.Client && \
+flatpack install -y flathub com.sublimetext.three && \
+flatpack install -y flathub org.telegram.desktop
--- install-elementary.sh
+++ install-elementary.sh
@@ -2,13 +2,11 @@
 
 # https://github.com/altdesktop/playerctl/releases/latest
 
-apt install -y vim zsh i3 i3status i3lock snapd python-pip python3-pip python-setuptools python3-setuptools virtualenv dmenu && \
+apt install -y vim zsh python-pip python3-pip python-setuptools python3-setuptools virtualenv && \
 	pip3 install thefuck && \
-	fuck ; fuck ; \
 	rsync -a home /home/$USER && \
-	rsync -a etc /etc && \
 	curl -L git.io/antigen > ~/.antigen/antigen.zsh && \
 	chsh -s $(which zsh) && \
         curl -s "https://get.sdkman.io" | bash && \
-	./snaps.sh
+	./flatpaks.sh
 
