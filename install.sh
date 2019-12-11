14173945773a1bb33679d2b00488f17f2892ef00
Ramazan Yapparov <ramazan.yapparov@x5.ru>
2019-12-11 10:41:36 +0300

    removed original files

--- install.sh
+++ install.sh
@@ -2,7 +2,9 @@
 
 # https://github.com/altdesktop/playerctl/releases/latest
 
-apt install -y vim zsh i3 i3status i3lock snapd python-pip python3-pip virtualenv dmenu && \
+apt install -y vim zsh i3 i3status i3lock snapd python-pip python3-pip python-setuptools python3-setuptools virtualenv dmenu && \
+	pip3 install thefuck && \
+	fuck ; fuck ; \
 	rsync -a home /home/$USER && \
 	rsync -a etc /etc && \
 	curl -L git.io/antigen > ~/.antigen/antigen.zsh && \
