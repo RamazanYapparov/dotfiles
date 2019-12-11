08a3a0dcb9a897bd44278af71f1d03d0e0fa8000
Ramazan Yapparov <ramazan.yapparov@gmail.com>
2019-12-11 11:34:59 +0300

    added readme

--- install-elementary.sh
+++ install-elementary.sh
@@ -2,8 +2,9 @@
 
 # https://github.com/altdesktop/playerctl/releases/latest
 
-apt install -y vim zsh python-pip python3-pip python-setuptools python3-setuptools virtualenv && \
+apt install -y vim zsh python-pip python3-pip python-setuptools python3-setuptools virtualenv flatpak && \
 	pip3 install thefuck && \
+    flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo && \
 	rsync -a home /home/$USER && \
 	curl -L git.io/antigen > ~/.antigen/antigen.zsh && \
 	chsh -s $(which zsh) && \
