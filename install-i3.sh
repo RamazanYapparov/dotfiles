214f80593bef734ad98f35f3d17ac545ee62d9e9
Ramazan Yapparov <ramazan.yapparov@gmail.com>
2019-12-13 12:44:40 +0300

    added screenshoting

--- install-i3.sh
+++ install-i3.sh
@@ -2,7 +2,7 @@
 
 # https://github.com/altdesktop/playerctl/releases/latest
 
-apt install -y vim zsh i3 i3status i3lock snapd python-pip python3-pip python-setuptools python3-setuptools virtualenv dmenu && \
+apt install -y vim zsh i3 i3status i3lock snapd python-pip python3-pip python-setuptools python3-setuptools virtualenv dmenu maim xclip && \
 	pip3 install thefuck && \
 	rsync -a home /home/$USER && \
 	rsync -a etc /etc && \
