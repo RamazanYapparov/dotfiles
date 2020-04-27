636cc95809e40ea1939ef5e8e45f4f9c41ba24c3
Ramazan Yapparov <ramazan.yapparov@gmail.com>
2020-04-28 01:11:24 +0300

    updated i3 configs

--- install-i3.sh
+++ install-i3.sh
@@ -2,7 +2,7 @@
 
 # https://github.com/altdesktop/playerctl/releases/latest
 
-sudo apt install -y vim zsh i3 i3status i3lock snapd python-pip python3-pip python-setuptools python3-setuptools virtualenv dmenu maim xclip py3status && \
+sudo apt install -y vim zsh i3 i3status i3lock snapd python-pip python3-pip python-setuptools python3-setuptools virtualenv dmenu maim xclip py3status dunst && \
 	sudo pip3 install thefuck && \
 	rsync -av home/ /home/$USER/ && \
 	sudo rsync -av etc/ /etc/ && \
